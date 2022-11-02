5acd2f1652f9172c2ac392003788c4932c0d0542
ioalexandros <ioalexandros@hotmail.com>
2022-11-02 16:29:21 +0200

    end of 2nov work

--- frontend/app/src/components/ChatBox.js
+++ frontend/app/src/components/ChatBox.js
@@ -1,15 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef ,useState } from 'react';
 import axiosConfig from '../rAPI/axios';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 import Emoji from './Emoji';
 
+const session = "https://dialogflow.googleapis.com/v2/projects/v-bot-9ktl/agent/sessions/2ecdf11f-2260-a02c-1b85-990164021ba2:detectIntent"
+
 function ChatBox(props) {
-  const [textCheckedShow, setTextCheckedShow] = useState(false)
+  const [textCheckedShow, setTextCheckedShow] = useState(false);
+  const [tipy, setTipy] = useState("");
+  const [searchQuery, setSearchQuery] = useState(null)
+  const [firstRender, setFirstRender] = useState(true)
 
   const bodyParameters = {
      "queryInput": {
         "text": {
-           "text": "hello",
+           "text": `${searchQuery}`,
            "languageCode": "en"
         }
      },
@@ -18,36 +24,47 @@ function ChatBox(props) {
   };
   
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await
-        axiosConfig.post(
-          bodyParameters
-        );
-      console.log(result)
-      //setData(result.data);
-    };
+    if(firstRender === false){
+      const fetchData = async () => {
+        const result = await
+          axiosConfig.post(
+            session,
+            bodyParameters
+          );
+        console.log(result)
+        //setData(result.data);
+      };
+
 
     fetchData();
-  }, []);
-  // [query]);
+    }
+    setFirstRender(false)
+  }, [searchQuery]);
+
+  const sendHandler  = e => {
+    const timer = setTimeout(() => 3000);
+    return true
+  }
 
   return (
     <div>
-      <input
-        type="text"
-        value={query}
-        onChange={event => setQuery(event.target.value)}
+      <TextField 
+        id="standard-basic" 
+        label="Standard" 
+        variant="filled"
+        value={tipy}
+        onChange={event => setTipy(event.target.value)}
       />
       <Button 
-              style={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}} 
-              variant="outlined"
               onClick={() => {
                         setTextCheckedShow(true); 
-                        setSearch(query)}
+                        setSearchQuery(tipy)
+                        setTipy("")
+                        }
                       }>
               <Emoji symbol="🐑" label="sheep"/>
       </Button>
-      {textCheckedShow && (<h2> Send</h2>) }
+      {textCheckedShow && sendHandler() && (<h2> Send</h2>) }
     </div>
 
     );
--- frontend/app/src/rAPI/axios.js
+++ frontend/app/src/rAPI/axios.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
 
-const token = "ya29.a0Aa4xrXMaVPWRk5XyQeNVa43IsKESIdFPGoGBufT0DwUPb0_feAG2d76_iDhuBDKR8ifCzDYCCDPp68XeyaY3BhLc1Rq_3gEtj88vdqT7cpDFv4Y10F36BinmX-RBqaON6mDuT2cztftf5mPN3DV_LcqxWk8eDbtQuwQATphgLi_tkvV7zWLoaiEYJiakemcBa_i-HGD_dkuGtac5EpG9SybIqL54rHFi95MyHrVYFweo5FgaCgYKATASARISFQEjDvL9mbdTtlbvcrUhr8nF2l5xng0246";
+const token = "ya29.a0Aa4xrXP5GhxfEgP7invY3af-jzlPoNGVokvIVm0fEAHgcSopWqaRiAxupqZ4p9_HmxUHrUL17Q8dqVrBUMYNvaWzvMXMx9mMOWalZVrmACdSTMc0u0Fs3GfPwOI7OTcirMrUBoaR5b880diDlLfHriUP8NqbTFC2Lx3rFmV7wdk39hTM1rvuT8g8PguMoq76oqO-pmvS065UPxmmAB-BB_T-sPxVKOWgt4JVOa_3CNeDa4caCgYKATASARISFQEjDvL9G-qGGLLRxhS-QerLX_xQHw0246";
 
 const instance = axios.create({
-	baseURL: 'https://dialogflow.googleapis.com/v2/projects/v-bot-9ktl/agent/sessions/2ecdf11f-2260-a02c-1b85-990164021ba2:detectIntent',
 	timeout: 1000,
-  	headers: {'Authorization': 'Bearer '+token}
+  	headers: {'Authorization': 'Bearer '+ token}
 });
 
-//instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
+instance.defaults.headers.common = {'Authorization': `Bearer ${token}`}
 
 //interceptors 
 
